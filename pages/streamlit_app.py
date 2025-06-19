5976314a9e6d5824797331a5eab2065d934b490d
Brandon B <bmbagley@gmail.com>
2025-06-19 13:07:54 -0400

    test adding hard coded image with credentials imbedded to url

--- pages/streamlit_app.py
+++ pages/streamlit_app.py
@@ -15,7 +15,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:", max_chars=100)
 st.write("The name on your smoothie will be: ", name_on_order)
 
-my_dataframe = st.dataframe("zenas_athleisure_db.products.catalog_for_website") #.select(col('FRUIT_NAME'), col('search_on'))
+st.image(image="https://sfc-prod3-ds1-86-customer-stage.s3.us-west-2.amazonaws.com/yoqz0000-s/stages/401f527e-4e80-44f4-bf6d-a94bb7c42d54/navy_blue_sweatsuit.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUIZLYB2N3S7XF256%2F20250619%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250619T170038Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=dafe48a12a73e8472015feffd919bd7f0c87b487a9ec4d300b641814123ae63c")
+# my_dataframe = st.dataframe("zenas_athleisure_db.products.catalog_for_website") #.select(col('FRUIT_NAME'), col('search_on'))
 
 # pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
